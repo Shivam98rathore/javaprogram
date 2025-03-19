7c7b33f5c558afd905663b340cef1db042befbae
Shivam Rathore <83808230+Shivam98rathore@users.noreply.github.com>
2025-03-19 10:37:56 +0530

    first program

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.time.LocalDate;
+import java.util.Date;
+
+public class main {
+    public static void main(String[] args) {
+       
+        System.out.println("this is my firsr program ");
+        System.out.println(LocalDate.now());
+        System.out.println(new Date());
+    
+    }
+
+}
