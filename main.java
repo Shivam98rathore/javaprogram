e44715cc9189424bbc831b106b31b481058a6949
Shivam Rathore <83808230+Shivam98rathore@users.noreply.github.com>
2025-03-19 10:50:37 +0530

    add sysout

--- main.java
+++ main.java
@@ -5,7 +5,7 @@ public class main {
     public static void main(String[] args) {
        
         System.out.println("hello world");
-        System.out.println("shivam");
+        System.out.println("shivam rathore");
         System.out.println(LocalDate.now());
         System.out.println(new Date());
         
