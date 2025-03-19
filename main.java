abf880d9be8a9a013d012b3a13bfbffc8096aca9
Shivam Rathore <83808230+Shivam98rathore@users.noreply.github.com>
2025-03-19 10:47:36 +0530

    update into main file

--- main.java
+++ main.java
@@ -5,8 +5,10 @@ public class main {
     public static void main(String[] args) {
        
         System.out.println("hello world");
+        System.out.println("shivam");
         System.out.println(LocalDate.now());
         System.out.println(new Date());
+        
     
     }
 
